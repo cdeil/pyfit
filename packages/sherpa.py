aa5ab69a7a95252bcbcb569aa4f84eebc2fb4c9a
Christoph Deil <Deil.Christoph@gmail.com>
2012-11-13 17:38:44 +0100

    Add notebook for fitting with sherpa (almost no content yet)

---
+++ packages/sherpa.py
@@ -0,0 +1,29 @@
+## Sherpa
+
+# CIAO's modelling and fitting package
+# 
+# http://cxc.cfa.harvard.edu/sherpa/
+
+# In[1]:
+# This is just an example to check if I can make the IPython notebook, nbconvert, sphinx and gh-pages play nice together.
+42 * 42
+
+# Out[1]:
+#     1764
+
+
+# In[2]:
+%pylab inline
+
+# Out[2]:
+#     
+#     Welcome to pylab, a matplotlib-based Python environment [backend: module://IPython.zmq.pylab.backend_inline].
+#     For more information, type 'help(pylab)'.
+# 
+# In[3]:
+plot(sin(arange(0, 10, 0.1)));
+
+# Out[3]:
+# image file: sherpa_files/sherpa_fig_00.png
+
+#     <matplotlib.figure.Figure at 0x1054accd0>
